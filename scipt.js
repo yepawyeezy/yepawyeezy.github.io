dca5ca5ef53ef3268ed4c0eee8d5cf0cbb6b30b6
yepawyeezy <145428624+yepawyeezy@users.noreply.github.com>
2023-10-20 15:58:45 +0700

    Add files via upload

---
+++ scipt.js
@@ -0,0 +1,42 @@
+// button hire me
+document.getElementById("tombol").addEventListener("click", function () {
+  window.location.href =
+    "https://www.linkedin.com/in/fariz-firmansyah-602599275/";
+});
+
+// box features
+document.getElementById("box1").addEventListener("click", function () {
+  window.location.href =
+    "https://www.youtube.com/channel/UCWXV17wcSYIdBoxLabAVXZQ";
+});
+
+document.getElementById("box2").addEventListener("click", function () {
+  window.location.href = "https://www.instagram.com/yaabeeww/";
+});
+
+document.getElementById("box3").addEventListener("click", function () {
+  window.location.href =
+    "https://www.youtube.com/channel/UCWXV17wcSYIdBoxLabAVXZQ";
+});
+
+document.getElementById("box4").addEventListener("click", function () {
+  window.location.href =
+    "https://mail.google.com/mail/u/0/#inbox?compose=GTvVlcSBmltWlVFtCrmrWLfWJJwWCMhMtBNbDKDbFssxlWDFFkgSXrqNWsxMrvMqtSMfqHPBfDqzg";
+});
+
+// button pricing
+
+document.getElementById("youtube1").addEventListener("click", function () {
+  window.location.href =
+    "https://www.youtube.com/channel/UCWXV17wcSYIdBoxLabAVXZQ";
+});
+
+document.getElementById("youtube2").addEventListener("click", function () {
+  window.location.href =
+    "https://www.youtube.com/channel/UCWXV17wcSYIdBoxLabAVXZQ";
+});
+
+document.getElementById("youtube3").addEventListener("click", function () {
+  window.location.href =
+    "https://www.youtube.com/channel/UCWXV17wcSYIdBoxLabAVXZQ";
+});
